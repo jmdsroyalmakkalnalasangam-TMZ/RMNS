4722b76f8535138591688766519e88ee20280ce4
jmdsroyalmakkalnalasangam-TMZ <jmdsroyalmakkalnalasangam@gmail.com>
2026-09-14 16:19:23 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA_KA6i-_xnXpCzNZOHHOS5ovabgPgCL6w",
+  authDomain: "rmns-website.firebaseapp.com",
+  databaseURL: "https://rmns-website-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "rmns-website",
+  storageBucket: "rmns-website.firebasestorage.app",
+  messagingSenderId: "323439403523",
+  appId: "1:323439403523:web:82cce0e3110fc47fa76436"
+};
